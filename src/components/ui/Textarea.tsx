d6b1f790ab07dd60742dee7a71139fa9baf63cd7
av-erencelik <erenmemo05@gmail.com>
2023-05-08 00:43:43 +0300

    updated shadcn ui

--- src/components/ui/Textarea.tsx
+++ src/components/ui/Textarea.tsx
@@ -1,14 +1,13 @@
-import * as React from "react";
+import * as React from "react"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  errorMessage: string | undefined;
-}
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ errorMessage, className, ...props }, ref) => {
-  return (
-    <>
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
       <textarea
         className={cn(
           "flex h-20 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
@@ -17,10 +16,9 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ errorMe
         ref={ref}
         {...props}
       />
-      <p className="mt-1 px-1 pb-2 text-center text-xs text-destructive">{errorMessage}</p>
-    </>
-  );
-});
-Textarea.displayName = "Textarea";
+    )
+  }
+)
+Textarea.displayName = "Textarea"
 
-export { Textarea };
+export { Textarea }
