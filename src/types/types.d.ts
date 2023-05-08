51e59916ed9f046a59277cca66be75c661b5bd71
av-erencelik <erenmemo05@gmail.com>
2023-05-08 18:59:34 +0300

    created zod schemas for db schemas

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,17 +1,4 @@
 import { z } from "zod";
 import { postSchema, signInSchema, signUpSchema } from "./schemas";
 
-export type SignUpFormData = z.infer<typeof signUpSchema>;
 export type SignInFormData = z.infer<typeof signInSchema>;
-export type PostData = z.infer<typeof postSchema>;
-
-export type UserWebhook = {
-  type: "user.created" | "user.deleted" | "user.updated";
-  data: {
-    id: string;
-    username: string;
-    email_addresses: {
-      email_address: string;
-    }[];
-  };
-};
