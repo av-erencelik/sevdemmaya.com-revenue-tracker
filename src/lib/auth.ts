68dd251b8efd46849e4f0371fdf482d69b657c0b
av-erencelik <erenmemo05@gmail.com>
2023-05-08 19:42:43 +0300

    deleted unnecessary functions and fixed useSignIn redirect url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,28 +1,6 @@
-import { SignInFormData, SignUpFormData } from "@/types/types";
-import { useSignIn, useSignUp } from "@clerk/nextjs";
+import { SignInFormData } from "@/types/types";
+import { useSignIn } from "@clerk/nextjs";
 import useSWRMutation from "swr/mutation";
-export const useSignUpClerk = () => {
-  const { signUp, setActive } = useSignUp();
-  const mutate = async (url: string, { arg }: { arg: SignUpFormData }) => {
-    return await signUp!
-      .create({
-        emailAddress: arg.email,
-        password: arg.password,
-        username: arg.username,
-        redirectUrl: "/",
-      })
-      .then((response) => {
-        if (response.status === "complete") {
-          console.log(response);
-          setActive?.({ session: response.createdSessionId });
-        }
-      })
-      .catch((error) => {
-        throw new Error(error.errors[0].message);
-      });
-  };
-  return useSWRMutation("signup", mutate);
-};
 
 export const useSignInClerk = () => {
   const { signIn, setActive } = useSignIn();
@@ -31,7 +9,7 @@ export const useSignInClerk = () => {
       .create({
         identifier: arg.identifier,
         password: arg.password,
-        redirectUrl: "/",
+        redirectUrl: "/admin/dashboard",
       })
       .then((response) => {
         if (response.status === "complete") {
